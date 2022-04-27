4571cb84f7ad9023891d77446bb5212eb82726d9
nacen <nacen.dev@gmail.com>
2022-04-28 00:04:34 +0800

    Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Header extends Component {
+  render() {
+    return (
+      <header className="p-4 bg-sky-700">
+        <h1 className="text-white text-2xl text-center lg:text-left">
+          CV Maker
+        </h1>
+      </header>
+    );
+  }
+}
