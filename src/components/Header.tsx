ea96ad4d2dcd178d2dbd5a3c7a5ddddfc889a213
nacen <nacen.dev@gmail.com>
2022-04-29 04:24:52 +0800

    Add print:hidden

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export class Header extends Component {
   render() {
     return (
-      <header className="p-4 bg-sky-700">
+      <header className="p-4 bg-sky-700 print:hidden">
         <h1 className="text-white text-2xl text-center lg:text-left">
           CV Maker
         </h1>
